b6da9c83b5999ef4d2d55c83c1942296f1fc8af0
Roman <romankos1993@gmail.com>
2023-05-10 19:44:24 +0300

    branch001 commit004

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
-    # path('blog/', include('landing.urls'))
+    path('', include('main.urls')),
+    path('', include('products.urls')),
+    path('', include('orders.urls'))
 ]
