0c2d48b0d6d4fa449233e53a24a9a2aff2e35c67
Roman <romankos1993@gmail.com>
2023-06-07 21:52:46 +0300

    branch001 commit011

--- orders/views.py
+++ orders/views.py
@@ -11,12 +11,19 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get("product_id")
     nmb = data.get("nmb")
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key,
-                                                                 product_id=product_id, defaults={"nmb": nmb})
-    if not created:
-        new_product.nmb += int(nmb)
-        new_product.save(force_update=True)
+    is_delete = data.get("is_delete")
 
+    if is_delete == 'true':
+        ProductInBasket.objects.filter(id=product_id).update(is_active=False)
+    else:
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
+                                                                     is_active=True, defaults={"nmb": nmb})
+        if not created:
+            print("not created")
+            new_product.nmb += int(nmb)
+            new_product.save(force_update=True)
+
+    # code for 2 cases
     products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     products_total_nmb = products_in_basket.count()
     return_dict["products_total_nmb"] = products_total_nmb
@@ -25,8 +32,10 @@ def basket_adding(request):
 
     for item in products_in_basket:
         product_dict = dict()
+        product_dict["id"] = item.id
         product_dict["name"] = item.product.name
         product_dict["price_per_item"] = item.price_per_item
         product_dict["nmb"] = item.nmb
         return_dict["products"].append(product_dict)
+
     return JsonResponse(return_dict)
