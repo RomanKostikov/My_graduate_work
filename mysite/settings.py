1a128d783f0474a3c515b21ba1941d40758fee24
Roman <romankos1993@gmail.com>
2024-01-14 14:59:52 +0300

    branch001 commit040

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,3 +159,38 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     from .settings_prod import *  # для дополнительных настроек
 # except:
 #     pass
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{levelname:<8}: {name:>25}] - {asctime} : {funcName} @\
+{lineno:04d}: {message}',
+            'style': '{',
+        },
+        'simple': {'format': '%(levelname)s %(message)s'},
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'logs' / 'django.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
+        'app_1': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
